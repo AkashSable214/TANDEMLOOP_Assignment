381386a8140df7dfed93ef21e461e15cb7ab3e8b
Akash Sable <122115230+AkashSable214@users.noreply.github.com>
2025-05-17 14:35:49 +0530

    Update Program-3.java

--- Program-3.java
+++ Program-3.java
@@ -1,1 +1,25 @@
+package test;
+
+import java.util.Scanner;
+
+public class CustomOddSeries {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of a: ");
+        int a = scanner.nextInt();
+
+        int count = (a % 2 == 0) ? a - 1 : a;
+
+        System.out.print("Output: ");
+        for (int i = 1; i <= count; i++) {
+            int odd = 2 * i - 1;
+            System.out.print(odd);
+            if (i < count) {
+                System.out.print(", ");
+            }
+        }
+    }
+}
+
 
