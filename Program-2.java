fb3b01992e8c30e6cce17925013e9191500d51ab
Akash Sable <122115230+AkashSable214@users.noreply.github.com>
2025-05-17 14:35:20 +0530

    Update Program-2.java

--- Program-2.java
+++ Program-2.java
@@ -1,1 +1,22 @@
+package test;
+
+import java.util.Scanner;
+
+public class OddNumberSeries {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of a: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Output: ");
+        for (int i = 1; i <= a; i++) {
+            int odd = 2 * i - 1;
+            System.out.print(odd);
+            if (i < a) {
+                System.out.print(", ");
+            }
+        }
+    }
+}
 
